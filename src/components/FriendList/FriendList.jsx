2ee5500ff130b20758af0ebfba089b58d5c52625
Liliya-ivanovna <lilyasviatenko@ukr.net>
2023-07-16 00:05:07 +0300

    update

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -5,19 +5,19 @@ export const FriendList=({friends})=>{
     return <ul className={css.friend_list}>
   {friends.map(({id,avatar,name,isOnline})=>(
     <li className={css.item} key={id}>
-    <span className={css.status}>{isOnline}</span>
+    <span className={`${css.status}${isOnline ? css.true : css.false}`}>{isOnline}</span>
     <img className={css.avatar} src={avatar} alt={name} width="48" />
     <p className={css.name}>{name}</p>
   </li>
   ))}
   </ul>
-};
+}; 
 
 FriendList.propTypes={
     friends:PropTypes.arrayOf(
         PropTypes.shape({
-            id: PropTypes.string.isRequired,
-            avatar:PropTypes.string.isRequired,
+            id: PropTypes.number.isRequired,
+            avatar:PropTypes.string,
             name:PropTypes.string.isRequired,
             isOnline:PropTypes.bool.isRequired,
         }),
