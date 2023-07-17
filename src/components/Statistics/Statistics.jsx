60447c5e6fffd58fbf8e5a93a631dc0acb491d17
Liliya-ivanovna <lilyasviatenko@ukr.net>
2023-07-17 12:19:52 +0300

    Update Statistics.jsx

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -6,8 +6,8 @@ export const Statistics=({title,stats})=>{
    {title && <h2 className={css.title}>{title}</h2> }
   <ul className={css.stat_list}>
      {stats.map(({id,label,percentage})=>(
-        <li key={id} className={`${css.item} ${css.backgroundColor =
-            Math.floor(Math.random() * 16777215).toString(16)}`}>
+        <li key={id} className={css.item}
+        style={{ backgroundColor: `#${Math.floor(Math.random() * 16777215).toString(16)}` }}>
        <span className={css.label}>{label}</span>
       <span className={css.percentage}>{percentage}%</span>
         </li>   
